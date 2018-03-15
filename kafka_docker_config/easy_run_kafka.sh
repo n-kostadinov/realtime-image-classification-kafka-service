8088a37e26cbae5a5ddb8ee970bcab36e41a18c9
n-kostadinov <nkkostadinov@gmail.com>
2018-03-15 20:17:24 +0100

    docker kafka updated

--- kafka_docker_config/easy_run_kafka.sh
+++ kafka_docker_config/easy_run_kafka.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo rm /tmp/* -R && sudo docker-compose rm && sudo docker-compose -f docker-compose-single-broker.yml up
+sudo docker-compose rm && sudo docker-compose -f docker-compose-single-broker.yml up
